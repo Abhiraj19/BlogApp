02aaa539c3eb8b9fa9a4e6e424877e38aa4d8268
Abhiraj19 <abhiabhiraj784@gmail.com>
2024-12-03 15:26:24 +0530

    appwrite setup done

---
+++ blogApp/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>
+        Blog App is started soon
+
+      </h1>
+
+  </>
+  )
+}
+
+export default App
